f21d8bec97bc27d6f3d9fa6e30addf69c31063a4
ingavisinskaite <ingavisinskaite@gmail.com>
2019-05-07 09:51:53 +0300

    initial commit

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,41 +1,138 @@
+import { Weather } from './../models/weather.model';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Weather } from '../models/weather.model';
 import { Observable } from 'rxjs';
+import { MaxLengthValidator } from '@angular/forms';
+import CitiesJson from '../../assets/city.continent.model.json';
+import * as moment from 'moment-timezone';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
+
 export class MainComponent implements OnInit {
 
   weather: string;
   temp: number;
-
+  temperature: string;
   selectedCity: string;
+  minTemp: string;
+  maxTemp: string;
+  windSpeed: string;
+  humidity: string;
+  sunrise: string;
+  sunset: string;
+  windDirection: string;
 
-  cities = ['Vilnius', 'Praha', 'Barcelona', 'Amsterdam', 'London', 'Paris', 'Tokyo', 'Moscow', 'Riga', 'Dubai'];
-
+  cities: any = CitiesJson;
 
   constructor(private _http: HttpClient) {
   }
 
   ngOnInit() {
+    console.log(CitiesJson);
   }
 
   getConfig(city: string): Promise<any> {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=3e430d76f0074ce63575d48b7fd38db7';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=3e430d76f0074ce63575d48b7fd38db7';
     return this._http.get(url).toPromise();
   }
 
   showWeather(event: string) {
-    console.log(event);
     this.getConfig(event)
-    .then(data => {
+    .then((data: Weather) => {
       console.log(data);
       this.weather = data.weather[0].main;
     });
   }
 
+  showTemperature(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      this.temperature = `${Math.round(data.main.temp)}°C`;
+    });
+  }
+
+  showMinTemp(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      this.minTemp = `${Math.round(data.main.temp_min)}°C / `;
+
+    });
+  }
+
+  showMaxTemp(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      this.maxTemp = `${Math.round(data.main.temp_max)}°C`;
+    });
+  }
+
+  showWind(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      this.windSpeed = `Speed: ${data.wind.speed} m/s`;
+    });
+  }
+
+  showWindDirection(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      if (data.wind.deg > 348.75 || (data.wind.deg < 78.75)) {
+        this.windDirection = 'Direction: North';
+      } else if(data.wind.deg < 168.75) {
+        this.windDirection = 'Direction: East';
+      } else if(data.wind.deg < 258.75) {
+        this.windDirection = 'Direction: South';
+      } else {
+        this.windDirection = 'Direction: West';
+      }
+    });
+  }
+
+  showHumidity(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      this.humidity = `${data.main.humidity}%`;
+    });
+  }
+
+  showSunrise(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      const x = data.sys.sunrise;
+      this.sunrise = `${this.getTimeStringFromMiliseconds(x)} / `;
+    });
+  }
+
+  showSunset(event: string) {
+    this.getConfig(event)
+    .then((data: Weather) => {
+      const x = data.sys.sunset;
+      this.sunset = this.getTimeStringFromMiliseconds(x);
+    });
+  }
+
+  getTimeStringFromMiliseconds(x: number) {
+    const hours = moment.unix(x).hours();
+    const mins = moment.unix(x).minutes();
+    const hoursString = `${hours < 10 ? '0' : ''}${hours}`; // add 0 if hrs < 10
+    const minsString = `${mins < 10 ? '0' : ''}${mins}`;
+    return `${hoursString}:${minsString}`;
+  }
+
+  showWeathers(event: string) {
+    this.showWeather(event);
+    this.showTemperature(event);
+    this.showMinTemp(event);
+    this.showMaxTemp(event);
+    this.showWind(event);
+    this.showSunrise(event);
+    this.showSunset(event);
+    this.showHumidity(event);
+    this.showWindDirection(event);
+  }
 }
--- src/app/models/weather.model.ts
+++ src/app/models/weather.model.ts
@@ -1,67 +1,46 @@
 export interface Weather {
-    city: City;
-    temperature: Temperature;
-    humidity: {
-        value: number;
-        unit: string;
-    };
-    wind: Wind;
-    pressure: {
-        value: number;
-        unit: string;
-    };
-    clouds: {
-        value: number;
-        name: string;
-    };
-    visibility: {
-        value: number;
-    };
-    precipitation: {
-        value: number;
-        mode: string;
-    };
-    weather: {
-        value: string;
-        number: number;
-        icon: string;
-    };
-    lastUpdate: {
-        value: string;
-    };
-}
-
-
-interface City {
-    id: string;
-    name: string;
     coord: {
-        lon: number;
+        long: number;
         lat: number;
     };
-    country: string;
-    sun: {
-        rise: number;
-        set: number;
+    weather: {
+        id: number;
+        main: string;
+        description: string;
+        icon: string;
     };
-}
-
-interface Temperature {
-    value: number;
-    min: number;
-    max: number;
-    unit: string;
-}
-
-interface Wind {
-    speed: {
-        value: number;
-        name: string;
+    base: string;
+    main: {
+        temp: number;
+        pressure: number;
+        humidity: number;
+        temp_min: number;
+        temp_max: number;
+        sea_level: number;
+        grnd_level: number;
     };
-    direction: {
-        value: number;
-        code: string;
-        name: string;
+    wind: {
+        speed: number;
+        deg: number;
     };
+    clouds: {
+        all: number
+    };
+    rain: {
+        _1h: number;
+        _3h: number;
+    };
+    dt: number;
+    sys: {
+        type: number;
+        id: number;
+        message: number;
+        country: string;
+        sunrise: number;
+        sunset: number;
+    };
+    id: number;
+    name: string;
+    cod: number;
 }
 
